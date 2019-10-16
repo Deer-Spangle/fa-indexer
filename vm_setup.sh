7ff7fd5f3efad85900fdfbe44d0258cddabef842
Deer-Spangle <deer@spangle.org.uk>
2019-10-16 22:01:50 +0100

    Adding some comment steps to the vm_setup.sh file

--- vm_setup.sh
+++ vm_setup.sh
@@ -1,3 +1,9 @@
+## First steps
+# 1) Create f1-micro VM, with debian 10
+# 2) Add ssh public key
+# 3) Copy this file to vm_setup.sh on the VM
+# 4) chmod +x vm_setup.sh
+# 5) ./vm_setup.sh
 sudo apt install git screen python3-pip
 echo "alias s='screen -dr'" > .bash_aliases
 git clone https://github.com/joshcoales/fa-indexer
