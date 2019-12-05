3ceffc8c69a073003526ef0acfa0e36b6b9ba643
Deer-Spangle <deer@spangle.org.uk>
2019-12-05 08:54:51 +0000

    Adding some more debug along the way

--- list_users.py
+++ list_users.py
@@ -3,13 +3,17 @@ import json
 
 files = glob.glob("D:/fa-indexer/data/*/*/*.json")
 users = set()
+total_files = len(files)
+count = 0
 for file in files:
     with open(file, "r") as f:
         d = json.load(f)
+        count += 1
     for key in d.keys():
         sub = d[key]
         if sub is not None:
             users.add(sub["username"])
+    print(f"Done {count} files of {total_files}. So far, {len(users)} users. Current file: {file}")
 
 print(f"total of {len(users)} users")
 with open("users.json", "w") as f:
